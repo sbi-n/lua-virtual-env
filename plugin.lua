a8829fc058d4a8acda1a554a885ea0c16583c700
SentryLN <156213411+SentryLN@users.noreply.github.com>
2025-02-24 20:19:04 +0900

    refactoring plugin

--- plugin.lua
+++ plugin.lua
@@ -6,28 +6,38 @@ local Interface = plugin:CreateToolbar("VM")
 local Button = Interface:CreateButton("Virtualize", "", "http://www.roblox.com/asset/?id=72875773659485")
 Button.ClickableWhenViewportHidden = true
 
-
+local Regulations = {
+	"%-%-%[.-%]%]",
+	"%-%-.-\n"
+}
+local function ClearDump(code)
+	local result = code
+	for steps, reg in pairs(Regulations) do
+		result = string.gsub(result, reg, "")
+	end
+	return result
+end
 
 
 Button.Click:Connect(function()
+	local Success, VM = pcall(function()
+		return HttpService:GetAsync("https://raw.githubusercontent.com/SentryLN/lua-virtual-env/refs/heads/main/main.lua")
+	end)
+	assert(Success, "failed to read virtual env")
 	for steps, victim in pairs(Selection:Get()) do
-		local Success, VM = pcall(function()
-			return HttpService:GetAsync("https://raw.githubusercontent.com/SentryLN/lua-virtual-env/refs/heads/main/main.lua")
-		end)
-		assert(Success, "failed to read virtual env")
-		VM ..= "\n\n"
+		VM = ClearDump(VM.."\n")
+		local lenVM = #VM
 
 		for index, value in pairs({victim, unpack(victim:GetDescendants())}) do
-			if value:IsA("Script") or value:IsA("ModuleScript") then
-				local code = value.Source:gsub("%-%-%[.-%]%]", "")
-				local len = #code
-				if len >= 200000-#VM then
-					warn( 200000-#VM)
-					warn(value:GetFullName(), "len:", len, ", length issue. virtualize skipped")
-					continue
-				end
-				value.Source = VM .. code
+			if not (value:IsA("Script") or value:IsA("ModuleScript")) then continue end
+			local code = ClearDump(value.Source)
+			local lenCode = #code
+			local lenTotal = lenVM+lenCode
+			if lenTotal >= 200000 then
+				warn(value:GetFullName(), "len:", lenTotal, ", length issue. virtualize skipped")
+				continue
 			end
+			value.Source = VM .. code
 		end
 	end
 	warn("all done!")
